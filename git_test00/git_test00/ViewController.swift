59a082555998a355e8f85d6ba5d70b584a2170a6
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-05-20 16:48:16 +0900

    test03

--- git_test00/git_test00/ViewController.swift
+++ git_test00/git_test00/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     //これこれテスト
     //これこれこれテストととと
     
+    //ちんちん
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
